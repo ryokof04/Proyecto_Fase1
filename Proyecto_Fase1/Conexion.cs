e91f77856998cdb2574b13496f7deb50e5156578
ryokof04 <100174385+ryokof04@users.noreply.github.com>
2023-05-30 20:22:33 -0700

    Clase Conexion

---
+++ Proyecto_Fase1/Conexion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Fase1
+{
+    internal class Conexion
+    {
+        string connectionString = "server=nombre_servidor;database=nombre_base_datos;uid=nombre_usuario;password=contraseña;";
+        string sqlQuery = "SELECT * FROM tabla";
+
+    }
+}
